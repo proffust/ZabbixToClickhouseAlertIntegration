5da7777d6b8cf3c990647b2de8030878c8f4aa42
proff.ust <proff.ust@gmail.com>
2019-10-23 18:51:08 +0300

    Initial commit

---
+++ event_to_click.py
@@ -0,0 +1,36 @@
+#!/etc/zabbix/alertscripts/venv_click/bin/python
+#-*- coding: utf-8 -*-
+import clickhouse_driver
+import argparse
+import json
+import datetime
+
+parser = argparse.ArgumentParser(description='Write zabbix events to clickhouse')
+parser.add_argument('alertsubject', type=int)
+parser.add_argument('alertmessage', type=str)
+parser.add_argument('--clickhouse-host', type=str, default="127.0.0.1")
+parser.add_argument('--clickhouse-port', type=int, default=9000)
+parser.add_argument('--clickhouse-db', type=str, default="zabbix")
+parser.add_argument('--clickhouse-table', type=str, default="events")
+args = parser.parse_args()
+
+event = json.loads(args.alertmessage)
+
+client = clickhouse_driver.Client(host=args.clickhouse_host, port=args.clickhouse_port,
+                database=args.clickhouse_db)
+
+if(args.alertsubject==1):
+    client.execute("INSERT INTO {0}.{1} (Clock, Eventid, Name, Groups, Hosts, Tags, Source, Severity) VALUES".format(
+                                                                            args.clickhouse_db, args.clickhouse_table),
+			    [{"Clock":int(datetime.datetime.strptime(event["datetime"], "%Y.%m.%d %H:%M:%S").timestamp()),
+                  "Eventid": event["eventid"],"Name": event["name"],"Groups":event["groups"].split(","),
+                  "Hosts": event["host"].split(","), "Tags":event["tags"].split(","), "Source": event["source"], "Severity": event["severity"]}])
+else:
+    client.execute("INSERT INTO {0}.{1} (Clock, RecoveryClock, Eventid, Name, Groups, Hosts, Tags, Source, Severity) VALUES".format(
+                                                                                            args.clickhouse_db, args.clickhouse_table),
+		    [{"Clock":int(datetime.datetime.strptime(event["startdatetime"], "%Y.%m.%d %H:%M:%S").timestamp()),
+		      "RecoveryClock": int(datetime.datetime.strptime(event["recoverydatetime"], "%Y.%m.%d %H:%M:%S").timestamp()),
+              "Eventid": event["eventid"], "Name": event["name"],"Groups":event["groups"].split(","),
+              "Hosts": event["host"].split(","), "Tags":event["tags"].split(","), "Source": event["source"], "Severity": event["severity"]}])
+
+client.disconnect()
