5a2deacd57363314256247f783d36f16733cf826
proff.ust <proff.ust@gmail.com>
2020-04-16 22:30:42 +0300

    fix #1, fix #2

--- event_to_click.py
+++ event_to_click.py
@@ -14,8 +14,12 @@ parser.add_argument('--clickhouse-db', type=str, default="zabbix")
 parser.add_argument('--clickhouse-table', type=str, default="events")
 args = parser.parse_args()
 
-event = json.loads(args.alertmessage)
-
+fixes = [('\xa0', ' '),('"',"'"),("{'",'{"'),("','", '","'),(",'", ',"'),("':'", '":"'),("':", '":'),("''", '\'\"'), ("'}",'"}')]
+alertmessage = args.alertmessage
+for fix in fixes:
+    alertmessage = alertmessage.replace(*fix)
+event = json.loads(alertmessage)
+print(event)
 client = clickhouse_driver.Client(host=args.clickhouse_host, port=args.clickhouse_port,
                 database=args.clickhouse_db)
 
@@ -23,14 +27,14 @@ if(args.alertsubject==1):
     client.execute("INSERT INTO {0}.{1} (Clock, Eventid, Name, Groups, Hosts, Tags, Source, Severity) VALUES".format(
                                                                             args.clickhouse_db, args.clickhouse_table),
 			    [{"Clock":int(datetime.datetime.strptime(event["datetime"], "%Y.%m.%d %H:%M:%S").timestamp()),
-                  "Eventid": event["eventid"],"Name": event["name"],"Groups":event["groups"].split(","),
-                  "Hosts": event["host"].split(","), "Tags":event["tags"].split(","), "Source": event["source"], "Severity": event["severity"]}])
+                  "Eventid": event["eventid"],"Name": event["name"],"Groups":event["groups"].replace(", ",",").split(","),
+                  "Hosts": event["host"].replace(", ",",").split(","), "Tags":event["tags"].split(","), "Source": event["source"], "Severity": event["severity"]}])
 else:
     client.execute("INSERT INTO {0}.{1} (Clock, RecoveryClock, Eventid, Name, Groups, Hosts, Tags, Source, Severity) VALUES".format(
                                                                                             args.clickhouse_db, args.clickhouse_table),
 		    [{"Clock":int(datetime.datetime.strptime(event["startdatetime"], "%Y.%m.%d %H:%M:%S").timestamp()),
 		      "RecoveryClock": int(datetime.datetime.strptime(event["recoverydatetime"], "%Y.%m.%d %H:%M:%S").timestamp()),
-              "Eventid": event["eventid"], "Name": event["name"],"Groups":event["groups"].split(","),
-              "Hosts": event["host"].split(","), "Tags":event["tags"].split(","), "Source": event["source"], "Severity": event["severity"]}])
+              "Eventid": event["eventid"], "Name": event["name"],"Groups":event["groups"].replace(", ",",").split(","),
+              "Hosts": event["host"].replace(", ",",").split(","), "Tags":event["tags"].split(","), "Source": event["source"], "Severity": event["severity"]}])
 
 client.disconnect()
